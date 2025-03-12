7d46022534544753525d29eac33eccab79028c16
parassareen4 <kyakrnahaibro@gmail.com>
2025-03-12 01:31:20 -0400

    dd

--- frontend/src/Landing.jsx
+++ frontend/src/Landing.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
     <iframe
-        src="/index.html"
+        src="/land.html"
         width="100%"
         height="100%"
         style={{ border: "none" }}
